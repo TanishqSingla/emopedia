3e2f3899d8f323ddacacc0ad817924cff66d260a
TanishqSingla <tanishqsingla@pop-os.localdomain>
2020-04-09 02:42:43 +0530

    basic design done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,13 @@ import React from "react";
 function App() {
   return <div className="App">
     <header>
+      <div className="nav">
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Contribute</a></li>
+        </ul>
+      </div>
       <h1>EmoPedia</h1>
     </header>
   </div>;

0d5137069a76a299db9fa3e5a131e779fe9e7235
TanishqSingla <tanishqsingla00@gmail.com>
2020-04-10 00:35:54 +0530

    code improvement

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,22 +4,22 @@ import Footer from "./Footer";
 import Card from "./Card";
 import emojis from "../data";
 
-function createCard(emoji) {
-  return (
-    <Card
-      key={emoji.id}
-      emote={emoji.emoji}
-      name={emoji.name}
-      define={emoji.meaning}
-    />
-  );
-}
-
 function App() {
   return (
     <div className="App">
       <Header />
-      <dl className="dictionary">{emojis.map(createCard)}</dl>
+      <dl className="dictionary">
+        {emojis.map((emoji) => {
+          return (
+            <Card
+              key={emoji.id}
+              emote={emoji.emoji}
+              name={emoji.name}
+              define={emoji.meaning}
+            />
+          );
+        })}
+      </dl>
       <Footer />
     </div>
   );
